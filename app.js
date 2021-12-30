cdce6dbe0d5d9fd074f83e79ff9b58b46de34615
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2021-12-29 22:47:00 -0800

    Modify images

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 const h2 = document.createElement("h2");
 h2.textContent = "An software engineer";
 
+const image = document.getElementById("myPicture");
+image.addEventListener('mouseover', function() {
+    image.src = 'images/batman.png';
+})
+
+image.addEventListener('mouseout', function() {
+    image.src = 'images/Santa-Enkhbat.png';
+})
+
 document.querySelector("body").appendChild(h2);
