4118ce625c9b125089be624fd924fe51fc2e2d4d
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2022-01-04 19:47:14 -0800

    Add h2 element directly on html file

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const h2 = document.createElement("h2");
-h2.textContent = "An software engineer";
+// const h2 = document.createElement("h2");
+// h2.textContent = "An software engineer";
 
 document.querySelector("body").appendChild(h2);
 
