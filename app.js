3e41a08de6c2b8c6bfdbf5ffb20cd27bf72ebc60
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2022-01-04 19:54:46 -0800

    Position profile image on center

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // const h2 = document.createElement("h2");
 // h2.textContent = "An software engineer";
 
-document.querySelector("body").appendChild(h2);
+// document.querySelector("body").appendChild(h2);
 
 const image = document.getElementById("myPicture");
 image.addEventListener('mouseover', function() {
