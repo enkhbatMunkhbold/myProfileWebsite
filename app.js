49349907132f8e13d5f11a642ba8fc571e30beb1
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2022-01-05 20:35:35 -0800

    Cosmetic changes on all containers

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
-// const h2 = document.createElement("h2");
-// h2.textContent = "An software engineer";
-
-// document.querySelector("body").appendChild(h2);
-
 const image = document.getElementById("myPicture");
 image.addEventListener('mouseover', function() {
     image.src = 'images/Santa-Enkhbat.png';
